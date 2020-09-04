1c2116895c5ac41d14997c1ce463192738a61fe2
emirgorkemozdemir <60661125+emirgorkemozdemir@users.noreply.github.com>
2020-09-04 16:11:43 +0300

    Add files via upload

---
+++ C Sharp Console Double/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Console_Double
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sayı1 = 8;
+
+            double sayı2 = 5.5;
+
+            double bölüm = sayı1 / sayı2;
+
+            Console.Write(bölüm);
+
+            Console.Read();
+        }
+    }
+}
