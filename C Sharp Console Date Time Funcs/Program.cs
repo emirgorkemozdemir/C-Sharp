fc1a82ce2c20a15a3ad2817d694285d79f4aa095
emirgorkemozdemir <60661125+emirgorkemozdemir@users.noreply.github.com>
2020-10-03 18:46:40 +0300

    Add files via upload

---
+++ C Sharp Console Date Time Funcs/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Console_Date_Time_Funcs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime zaman1 = DateTime.Now;
+
+            DateTime zaman2 = new DateTime(2018,12,25,19,50,50);
+
+            double fark = zaman1.Subtract(zaman2).TotalSeconds;
+
+            Console.WriteLine(zaman1);
+
+            Console.WriteLine(zaman2);
+
+            Console.WriteLine(fark);
+
+            Console.Read();
+        }
+    }
+}
